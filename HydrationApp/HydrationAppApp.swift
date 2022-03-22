a8f572ecc8a720df6e77180857b2ac599ffbda8c
costa-rica <nrodrig1@gmail.com>
2022-03-22 08:41:34 +0100

    Initial Commit

---
+++ HydrationApp/HydrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationAppApp.swift
+//  HydrationApp
+//
+//  Created by Nick on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct HydrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
